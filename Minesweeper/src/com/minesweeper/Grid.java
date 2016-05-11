5c73577909458b0c4805d6dc6900e020a1819647
beerlow <lam_romain@hotmail.fr>
2016-05-11 17:30:59 +0200

    Click on figure test completed

--- Minesweeper/src/com/minesweeper/Grid.java
+++ Minesweeper/src/com/minesweeper/Grid.java
@@ -5,7 +5,7 @@ public class Grid {
 
 	public Grid(Cell[][] grid) {
 		this.grid = grid;
-		
+		initializeCases();
 	}
 
 	public Cell[][] getGrid() {
@@ -21,4 +21,36 @@ public class Grid {
 			throw new IllegalArgumentException("Invalid position");
 		return !grid[l][c].isMine();
 	}
+	private void initializeCases() {
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				if (grid[i][j] == null) {
+					grid[i][j] = new Cell();
+				}
+				if (!grid[i][j].isMine()) {
+					//Calculate the number of adjacent mines
+					grid[i][j].setNumberOfAdjacentMines(MineOnCase(i + 1, j)
+							+ MineOnCase(i - 1, j + 1)
+							+ MineOnCase(i + 1, j + 1)
+							+ MineOnCase(i + 1, j - 1)
+							+ MineOnCase(i - 1, j - 1) + MineOnCase(i, j - 1)
+							+ MineOnCase(i - 1, j) + MineOnCase(i, j + 1));
+				}
+
+			}
+		}
+
+	}
+	private int MineOnCase(int i, int j) {
+		if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length) {
+			if (grid[i][j].isMine())
+				return 1;
+			else
+				return 0;
+		} else {
+			return 0;
+		}
+
+	}
+
 }
