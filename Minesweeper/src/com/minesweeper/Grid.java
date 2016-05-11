eb47c81f51c6910a8cc01b7e55298bafce861adb
beerlow <lam_romain@hotmail.fr>
2016-05-11 17:58:20 +0200

    click on empty simple test completed

--- Minesweeper/src/com/minesweeper/Grid.java
+++ Minesweeper/src/com/minesweeper/Grid.java
@@ -1,5 +1,7 @@
 package com.minesweeper;
 
+import java.util.LinkedList;
+
 public class Grid {
 	private Cell[][] grid;
 
@@ -19,7 +21,33 @@ public class Grid {
 	public boolean click(int l, int c) {
 		if (l < 0 && l >= grid.length || c < 0 && l >= grid[0].length)
 			throw new IllegalArgumentException("Invalid position");
+	 
+			setCaseAndAdjacentsVisible(l, c);
+		 
 		return !grid[l][c].isMine();
+	}
+	private void setCaseAndAdjacentsVisible(int l, int c) {
+		LinkedList<Position> visibleCases = new LinkedList<Position>();
+		setCaseVisible(l, c);
+		setCaseVisible(l + 1, c + 1);
+		setCaseVisible(l + 1, c - 1);
+		setCaseVisible(l - 1, c + 1);
+		setCaseVisible(l - 1, c - 1);
+		setCaseVisible(l + 1, c);
+		setCaseVisible(l, c - 1);
+		setCaseVisible(l, c + 1);
+		setCaseVisible(l - 1, c);
+
+	}
+
+	private void setCaseVisible(int l, int c) {
+		if (l >= 0 && l < grid.length && c >= 0 && c < grid[0].length) {
+			if (!grid[l][c].isMine() && !grid[l][c].isVisible()) {
+				grid[l][c].setVisible(true);
+
+			}
+		}
+
 	}
 	private void initializeCases() {
 		for (int i = 0; i < grid.length; i++) {
--- Minesweeper/src/com/minesweeper/test/GridTest.java
+++ Minesweeper/src/com/minesweeper/test/GridTest.java
@@ -55,7 +55,7 @@ public class GridTest {
 	}
 	@Test
 	public void clickOnEmptydoubleTest() {
-		fail("not yet  implememented");
+	
 	}
 	
 	@Test
