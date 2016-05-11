841ba1b6c694129e06b1320467bb3666f534ee61
beerlow <lam_romain@hotmail.fr>
2016-05-11 16:37:09 +0200

    Creation de la grille et des premiers tests

---
+++ Minesweeper/src/com/minesweeper/Grid.java
@@ -0,0 +1,7 @@
+package com.minesweeper;
+
+ 
+
+public class Grid {
+	
+}
---
+++ Minesweeper/src/com/minesweeper/test/GridTest.java
@@ -0,0 +1,32 @@
+package com.minesweeper.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GridTest {
+
+
+	@Test
+	public void getGridTest() {
+		fail("not yet  implememented");
+	}
+	@Test
+	public void clickOnMineTest() {
+		fail("not yet  implememented");
+		
+	}
+	@Test
+	public void clickOnEmptySimpleTest() {
+		fail("not yet  implememented");
+	}
+	@Test
+	public void clickOnEmptydoubleTest() {
+		fail("not yet  implememented");
+	}
+	@Test
+	public void clickOnFigureTest() {
+		fail("not yet  implememented");
+	}
+
+}
